57cf7bf84884be43794d8449c9b376e142ca7c24
Khizer Hayat <khizerqureshi123@gmail.com>
2024-01-05 03:26:28 +0500

    fourth commit fetch hook completed

--- Client/src/Components/featuredProperties/featuredProperties.jsx
+++ Client/src/Components/featuredProperties/featuredProperties.jsx
@@ -1,57 +1,35 @@
 import useFetch from "../../hooks/useFetch";
-import "./featuredProperties.css"
+import "./featuredProperties.css";
 
 const FeaturedProperties = () => {
+  const { data, loading, error } = useFetch("/api/hotels?featured=true&limit=4");
 
-    
-
-    return (
-        <div className="fp">
-            <div className="fpItem">
-            <img src="https://cf.bstatic.com/xdata/images/hotel/square600/13125860.webp?k=35b70a7e8a17a71896996cd55d84f742cd15724c3aebaed0d9b5ba19c53c430b&o=" alt="" className="fpImg" />
-            <span className="fpName">Aparthotel Stare Miasto</span>
-            <span className="fpCity">Old Town, Poland, Krakow</span>
-            <span className="fpPrice">Starting from PKR 29,022</span>
-            <div className="fpRating">
-                <button>8.9</button>
+  return (
+    <div className="fp">
+      {loading ? (
+        "Loading"
+      ) : (
+        <>
+          {data.map((item) => (
+            <div className="fpItem" key={item._id}>
+              <img
+                src={item.photos[0]}
+                alt=""
+                className="fpImg"
+              />
+              <span className="fpName">{item.name}</span>
+              <span className="fpCity">{item.city}</span>
+              <span className="fpPrice">Starting from ${item.cheapestPrice}</span>
+              {item.rating && <div className="fpRating">
+                <button>{item.rating}</button>
                 <span>Excellent</span>
+              </div>}
             </div>
-            </div>
+          ))}
+        </>
+      )}
+    </div>
+  );
+};
 
-            <div className="fpItem">
-            <img src="https://cf.bstatic.com/xdata/images/hotel/square600/13125860.webp?k=35b70a7e8a17a71896996cd55d84f742cd15724c3aebaed0d9b5ba19c53c430b&o=" alt="" className="fpImg" />
-            <span className="fpName">Aparthotel Stare Miasto</span>
-            <span className="fpCity">Old Town, Poland, Krakow</span>
-            <span className="fpPrice">Starting from PKR 29,022</span>
-            <div className="fpRating">
-                <button>8.9</button>
-                <span>Excellent</span>
-            </div>
-            </div>
-
-            <div className="fpItem">
-            <img src="https://cf.bstatic.com/xdata/images/hotel/square600/13125860.webp?k=35b70a7e8a17a71896996cd55d84f742cd15724c3aebaed0d9b5ba19c53c430b&o=" alt="" className="fpImg" />
-            <span className="fpName">Aparthotel Stare Miasto</span>
-            <span className="fpCity">Old Town, Poland, Krakow</span>
-            <span className="fpPrice">Starting from PKR 29,022</span>
-            <div className="fpRating">
-                <button>8.9</button>
-                <span>Excellent</span>
-            </div>
-            </div>
-
-            <div className="fpItem">
-            <img src="https://cf.bstatic.com/xdata/images/hotel/square600/13125860.webp?k=35b70a7e8a17a71896996cd55d84f742cd15724c3aebaed0d9b5ba19c53c430b&o=" alt="" className="fpImg" />
-            <span className="fpName">Aparthotel Stare Miasto</span>
-            <span className="fpCity">Old Town, Poland, Krakow</span>
-            <span className="fpPrice">Starting from PKR 29,022</span>
-            <div className="fpRating">
-                <button>8.9</button>
-                <span>Excellent</span>
-            </div>
-            </div>
-        </div>
-    )
-}
-
-export default FeaturedProperties
+export default FeaturedProperties;
